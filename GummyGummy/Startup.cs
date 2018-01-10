0cf943a3111e377350db75e19ee6626d7cf748c4
jkcherrelus <jkcherrelus6038@eagle.fgcu.edu>
2018-01-10 11:36:16 -0500

    Add project files.

---
+++ GummyGummy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GummyGummy.Startup))]
+namespace GummyGummy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
